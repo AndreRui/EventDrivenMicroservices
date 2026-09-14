e997583b2009832ca41acd76c651135619ea6062
AndyRuiz <194693346+AndreRui@users.noreply.github.com>
2026-09-14 20:16:04 +0000

    indexController

--- apps/platform-engine/src/main/java/com/eventdrivenmicroservices/platform/api/IndexController.java
+++ apps/platform-engine/src/main/java/com/eventdrivenmicroservices/platform/api/IndexController.java
@@ -1,13 +1,20 @@
 package com.eventdrivenmicroservices.platform.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "forward:/index.html";
+    public Mono<Void> index(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create("/index.html"));
+        return response.setComplete();
     }
 }
